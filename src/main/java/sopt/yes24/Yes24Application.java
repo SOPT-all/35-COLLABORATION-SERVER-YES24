7bd87be7bdd7089ef21b0dea0d6a83b1648c4d59
dajinYang <didekwls0104@naver.com>
2024-11-18 23:44:18 +0900

    initial settings

---
+++ src/main/java/sopt/yes24/Yes24Application.java
@@ -0,0 +1,13 @@
+package sopt.yes24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yes24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yes24Application.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/yes24/Yes24ApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.yes24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yes24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
