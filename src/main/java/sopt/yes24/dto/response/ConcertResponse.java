d8cd8f255ea77cad0f5686e2cfcddcc774c40b9c
dajinYang <didekwls0104@naver.com>
2024-11-21 22:19:44 +0900

    chore : ConcertResponse에 포맷한 Date 추가

--- src/main/java/sopt/yes24/dto/response/ConcertResponse.java
+++ src/main/java/sopt/yes24/dto/response/ConcertResponse.java
@@ -2,11 +2,17 @@ package sopt.yes24.dto.response;
 
 import sopt.yes24.entity.Ticket;
 
-public record ConcertResponse (Long concert_id, String concert_title, String concert_area, String concert_img){
+import java.util.Date;
+
+public record ConcertResponse (Long concert_id, String concert_title, String concert_date, String concert_area, String concert_img){
+
     public static ConcertResponse fromEntity(Ticket ticket) {
+        String formattedDate = ticket.getDate().toString();
+
         return new ConcertResponse(
                 ticket.getTicketId(),
                 ticket.getTitle(),
+                formattedDate,
                 ticket.getArea(),
                 ticket.getImg()
         );
